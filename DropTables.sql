b67fceb0a97660ce459793c9a732992dc4f1e021
quarlow <30605595+quarlow@users.noreply.github.com>
2019-05-31 09:48:20 -0600

    Script to drop tables
    added comments

--- DropTables.sql
+++ DropTables.sql
@@ -1,3 +1,6 @@
+-- Due to the errors and issues I found when creating the tables, and altering the schema, I frequently dropped the tables 
+-- and started over.  I used this script to drop all the tables in one action.
+
 drop table osm.ways_tags;
 drop table osm.ways_nodes;
 drop table osm.nodes_tags;
